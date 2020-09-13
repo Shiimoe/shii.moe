2f186405f7a746ed805b56ac82ee14b8adb0e5d7
Shiimoe <SameulFrost@tuta.io>
2020-09-13 21:25:01 +0100

    yui!

--- image.js
+++ image.js
@@ -1,2 +1,2 @@
 const mascot = document.getElementById("mascot");
-mascot.setAttribute("src", "/mascots/" + Math.floor(1 + Math.random()*9) + ".png");
+mascot.setAttribute("src", "/mascots/" + Math.floor(1 + Math.random()*14) + ".png");
