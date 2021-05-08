832096e63327cd4e2b5b0881674e77ffc12cae2b
knutsen <samuel@knutsen.co>
2021-05-08 22:21:12 +0100

    Use absolute paths for mascots.

--- image.js
+++ image.js
@@ -2,7 +2,7 @@ const mascot = document.getElementById("mascot");
 const favicon = document.querySelector("link[rel~='icon']");
 
 const randomYui = () => {
-	const stem = "mascots/" + Math.floor(1 + Math.random()*14);
+	const stem = "/mascots/" + Math.floor(1 + Math.random()*14);
 	mascot.setAttribute("src", stem + ".png");
 	favicon.setAttribute("href", stem + "-square.png");
 }
