b20c91be88b832d57847ee52c23763d86dd7cd2a
knutsen <samuel@knutsen.co>
2021-06-24 12:08:13 +0100

    Don't show mascot until loaded (JS enabled only)

--- public/image.js
+++ public/image.js
@@ -2,12 +2,18 @@ const YUI_COUNT = 17;
 
 const mascot = document.getElementById("mascot");
 const favicon = document.querySelector("link[rel~='icon']");
+mascot.style.opacity = 0;
 
 const randomYui = () => {
 	const pick = Math.floor(1 + Math.random() * YUI_COUNT);
 	const stem = "/mascots/" + pick;
+	mascot.style.opacity = 0;
 	mascot.setAttribute("src", stem + ".png");
 	favicon.setAttribute("href", stem + "-square.png");
+
+	const loaded = () => { mascot.style.opacity = .85 };
+	if (mascot.complete) loaded();
+	else mascot.addEventListener('load', loaded);
 
 	useColourScheme({
 		11: 'pink',
