0196de9615343a5d7325baba9e9c5c6e9d287135
knutsen <samuel@knutsen.co>
2021-07-02 00:10:41 +0100

    Smooth load mascots, noscript makes them always visible.

--- public/image.js
+++ public/image.js
@@ -1,25 +1,27 @@
-const YUI_COUNT = 17;
-const NOVELTY = [11, 15, 16];
+const MASCOT_COUNT = 17;
+const MASCOTS = [...new Array(MASCOT_COUNT)].map((_, i) => i + 1)
+const NOVELTY = [11, 15, 16];  //< Novelty/special mascots.
 
 const mascot = document.getElementById("mascot");
 const favicon = document.querySelector("link[rel~='icon']");
-mascot.style.opacity = 0;
 
-const randomYui = (excludeList=[]) => {
-	if (!Array.isArray(excludeList))
-		excludeList = [];
-	const picks = [...new Array(YUI_COUNT)]
-		.map((_, i) => i + 1)
-		.filter(e => !excludeList.includes(e));
-	const pick = picks[Math.floor(Math.random() * picks.length)];
-	const stem = "/mascots/" + pick;
+const setMascot = (pick) => {
+	const stem = `/mascots/${pick}`;
 	mascot.style.opacity = 0;
-	mascot.setAttribute("src", stem + ".png");
-	favicon.setAttribute("href", stem + "-square.png");
+	mascot.setAttribute('src', `${stem}.png`);
+	favicon.setAttribute('href', `${stem}-square.png`);
 
 	const loaded = () => { mascot.style.opacity = .85 };
 	if (mascot.complete) loaded();
 	else mascot.addEventListener('load', loaded);
+}
+
+const randomMascot = (excludeList=[]) => {
+	if (!Array.isArray(excludeList)) excludeList = [];
+
+	const picks = MASCOTS.filter(e => !excludeList.includes(e));
+	const pick = picks[Math.floor(Math.random() * picks.length)];
+	setMascot(pick);
 
 	useColourScheme({
 		11: 'pink',
@@ -28,4 +30,4 @@ const randomYui = (excludeList=[]) => {
 	}[pick] || 'default');
 }
 
-mascot.addEventListener("click", randomYui)
+mascot.addEventListener('click', randomMascot)
