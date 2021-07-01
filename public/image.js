1e2fc11f2978cda7a238f41ff93180d0721f3d62
Shiimoe <samuelfrost@tuta.io>
2021-07-01 21:44:53 +0100

    Exclude novelty mascots on initial page load

--- public/image.js
+++ public/image.js
@@ -1,11 +1,17 @@
 const YUI_COUNT = 17;
+const NOVELTY = [11, 15, 16];
 
 const mascot = document.getElementById("mascot");
 const favicon = document.querySelector("link[rel~='icon']");
 mascot.style.opacity = 0;
 
-const randomYui = () => {
-	const pick = Math.floor(1 + Math.random() * YUI_COUNT);
+const randomYui = (excludeList=[]) => {
+	if (!Array.isArray(excludeList))
+		excludeList = [];
+	const picks = [...new Array(YUI_COUNT)]
+		.map((_, i) => i + 1)
+		.filter(e => !excludeList.includes(e));
+	const pick = picks[Math.floor(Math.random() * picks.length)];
 	const stem = "/mascots/" + pick;
 	mascot.style.opacity = 0;
 	mascot.setAttribute("src", stem + ".png");
